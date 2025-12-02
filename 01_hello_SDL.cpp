057ce0c9c48e21b499981942d549622c81f4795f
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2025-12-02 22:29:21 +0800

    Make rect

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -36,13 +36,21 @@ int main( int argc, char* args[] )
 			screenSurface = SDL_GetWindowSurface( window );
 
 			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+			// SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+			SDL_Rect fillRect{ 10, 20, 30, 40 };
+			SDL_FillRect( screenSurface, &fillRect, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
 			
 			//Update the surface
 			SDL_UpdateWindowSurface( window );
             
             //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+            SDL_Event e;
+			bool quit = false;
+			while( quit == false ){
+				while( SDL_PollEvent( &e ) ){
+					if( e.type == SDL_QUIT ) quit = true; 
+				} 
+			}
 		}
 	}
 
