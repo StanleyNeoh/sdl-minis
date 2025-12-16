36df6e0cd941d81bf7f45ab9cc703847b41e0312
stanleyneoh <neohstanley@gmail.com>
2025-12-17 02:53:17 +0800

    Finish connect4 game

--- 3-connect4/lib/connect4.hpp
+++ 3-connect4/lib/connect4.hpp
@@ -6,29 +6,98 @@
 #include "viewport.hpp"
 #include "utils.hpp"
 
+struct Cell {
+    using CellKey = char;
+    constexpr static CellKey PlayerNoneKey = '.';
+    constexpr static CellKey PlayerBlueKey = 'B';
+    constexpr static CellKey PlayerRedKey = 'R';
+    constexpr static Color WHITE{255, 255, 255};
+    constexpr static Color RED{255, 0, 0};
+    constexpr static Color BLUE{0, 0, 255};
+
+    char key;
+    bool marked;
+
+    Cell(): Cell(PlayerNoneKey) {}
+    Cell(char c): key(c), marked(false) {}
+
+    void set(CellKey k) {
+        key = k;
+    }
+
+
+    friend std::ostream& operator<<(std::ostream& o, const Cell& p) {
+        if (p.marked) {
+            o << '[' << p.key << ']';
+        } else {
+            o << ' ' << p.key << ' ';
+        }
+        return o;
+    }
+
+    bool operator==(const CellKey& k) {
+        return key == k;
+    }
+
+    bool operator!=(const CellKey& k) {
+        return key != k;
+    }
+
+    void mark() {
+        marked = true;
+    }
+
+    void unmark() {
+        marked = false;
+    }
+
+    bool isMarked() {
+        return marked;
+    }
+
+    uint32_t getSDLColor(SDL_PixelFormat* format) {
+        uint32_t color;
+        switch(key) {
+        case PlayerBlueKey:
+            return mapColor(format, BLUE);
+            break;
+        case PlayerRedKey:
+            return mapColor(format, RED);
+            break;
+        default:
+            return mapColor(format, WHITE);
+            break;
+        };
+    }
+};
+
+
 template <int M, int N>
 struct Board: public ViewPort<Board<M, N>> {
     using Par = ViewPort<Board<M, N>>;
     using Par::rect;
     using Par::tex;
     using Par::format;
-    using Grid = std::array<std::array<Player, N>, M>;
+    using Grid = std::array<std::array<Cell, N>, M>;
+
+    static constexpr Color BACKGROUND{0, 0, 100};
+    static constexpr Color HIGHLIGHT{0, 255, 0};
     
     Grid grid;
     int br = 20;
-    Player turn = Player::PlayerRed;
-    Player winner = Player::None;
+    Cell::CellKey turn = Cell::PlayerRedKey;
+    Cell::CellKey winner = Cell::PlayerNoneKey;
     int col_i = -1;
 
     Board() {
         for (int r = 0; r < M; r++) {
             for (int c = 0; c < N; c++) {
-                grid[r][c] = Player::None;
+                grid[r][c] = Cell();
             }
         }
     }
 
-    bool draw_board(int br, Color bc) {
+    bool draw(SDL_Renderer* renderer) {
         uint32_t* pixels;
         int pitch;
         if (SDL_LockTexture(tex, NULL, reinterpret_cast<void**>(&pixels), &pitch) != 0) {
@@ -40,21 +109,22 @@ struct Board: public ViewPort<Board<M, N>> {
         float cwr = cw / 2.0;
         int ch = bw / M;
         float chr = ch / 2.0;
-        uint32_t color = map_color(format, bc);
+        uint32_t bgColor = mapColor(format, BACKGROUND);
+        uint32_t hlColor = mapColor(format, HIGHLIGHT);
 
         for (int r = 0; r < br; r++) {
             int dy = br - r;
             int dx = br - SDL_sqrt(br * br - dy * dy);
-            uint32_t* toppad = unsafe_shift(pixels, r * pitch);
-            uint32_t* botpad = unsafe_shift(pixels, (rect.h - 1 - r) * pitch);
+            uint32_t* toppad = unsafeShift(pixels, r * pitch);
+            uint32_t* botpad = unsafeShift(pixels, (rect.h - 1 - r) * pitch);
             for (int c = dx; c < rect.w - dx; c++) {
-                botpad[c] = toppad[c] = color;
+                botpad[c] = toppad[c] = bgColor;
             }
         }
         for (int r = br; r < rect.h - br; r++) {
-            uint32_t* rowpix = unsafe_shift(pixels, r * pitch);
+            uint32_t* rowpix = unsafeShift(pixels, r * pitch);
             for (int c = 0; c < rect.w; c++) {
-                rowpix[c] = color;
+                rowpix[c] = bgColor;
             }
         }
         for (int r = 0; r < M; r++) {
@@ -63,36 +133,24 @@ struct Board: public ViewPort<Board<M, N>> {
                 int tly = br + ch * r;
                 float cx = tlx + cwr;
                 float cy = tly + chr;
-                uint32_t color;
-                switch(grid[r][c]) {
-                case Player::PlayerBlue:
-                    color = map_color(format, BLUE);
-                    break;
-                case Player::PlayerRed:
-                    color = map_color(format, RED);
-                    break;
-                default:
-                    color = map_color(format, WHITE);
-                    break;
-                };
+                uint32_t pColor = grid[r][c].getSDLColor(format);
+                bool isMarked = grid[r][c].isMarked();
                 for (int y = tly; y < tly + ch; y++) {
-                    uint32_t* rowpix = unsafe_shift(pixels, y * pitch);
+                    uint32_t* rowpix = unsafeShift(pixels, y * pitch);
                     for (int x = tlx; x < tlx + cw; x++) {
                         float dx = (x - cx) / cwr;
                         float dy = (y - cy) / chr;
-                        if (dx * dx + dy * dy <= 0.8) {
-                            rowpix[x] = color;
+                        float d = dx * dx + dy * dy;
+                        if (d <= 0.8) {
+                            rowpix[x] = pColor;
+                        } else if (isMarked && d <= 0.9) {
+                            rowpix[x] = hlColor;
                         }
                     }
                 }
             }
         }
         SDL_UnlockTexture(tex);
-        return true;
-    }
-
-    bool draw(SDL_Renderer* renderer) {
-        draw_board(br, BOARD_BG);
         return Par::draw(renderer);
     }
 
@@ -107,47 +165,45 @@ struct Board: public ViewPort<Board<M, N>> {
     }
 
     void handle_mouse_up(const SDL_MouseButtonEvent& e) {
-        std::cout << "Click " <<  col_i << std::endl;
+        if (winner != Cell::PlayerNoneKey) return;
         drop_piece(col_i);
+        winner = check_win();
     }
 
     void switch_turn() {
         switch(turn) {
-        case Player::PlayerRed:
-            turn = Player::PlayerBlue;
+        case Cell::PlayerBlueKey:
+            turn = Cell::PlayerRedKey;
             break;
-        case Player::PlayerBlue:
-            turn = Player::PlayerRed;
+        case Cell::PlayerRedKey:
+            turn = Cell::PlayerBlueKey;
+            break;
+        default:
             break;
         }
-        std::cout << "Switched to " << turn << "\n";
     }
 
     bool drop_piece(int col_i) {
         for (int r = M-1; r >= 0; r--) {
-            if (grid[r][col_i] != Player::None) continue;
-            grid[r][col_i] = turn;
-            std::cout << grid <<  "=> " << r << " " << col_i << " " << turn << "\n";
+            if (grid[r][col_i] != Cell::PlayerNoneKey) continue;
+            grid[r][col_i].set(turn);
+            std::cout << grid << "\n";
             switch_turn();
             return true;
         }
         return false;
     }
 
-    Player check_win(int req = 4) {
+    Cell::CellKey check_win(int req = 4) {
         // Check row
         for (int r = 0; r < M; r++) {
-            Player pp = Player::None;
+            Cell::CellKey pp = Cell::PlayerNoneKey;
             int pc = 0;
             for (int c = 0; c < N; c++) {
-                Player p = grid[r][c];
-                if (p == pp) {
-                    pc++;
-                } else {
-                    pp = p;
-                    pc = 1;
-                }
-                if (pc >= req && pp != Player::None) {
+                if (check_loop(req, grid[r][c], pp, pc)) {
+                    for (int _c = c - req + 1; _c <= c; _c++) {
+                        grid[r][_c].mark();
+                    }
                     return pp;
                 }
             }
@@ -155,23 +211,67 @@ struct Board: public ViewPort<Board<M, N>> {
 
         // Check col
         for (int c = 0; c < N; c++) {
-            Player pp = Player::None;
+            Cell::CellKey pp = Cell::PlayerNoneKey;
             int pc = 0;
             for (int r = 0; r < M; r++) {
-                Player p = grid[r][c];
-                if (p == pp) {
-                    pc++;
-                } else {
-                    pp = p;
-                    pc = 1;
-                }
-                if (pc >= req && pp != Player::None) {
+                if (check_loop(req, grid[r][c], pp, pc)) {
+                    for (int _r = r - req + 1; _r <= r; _r++) {
+                        grid[_r][c].mark();
+                    }
                     return pp;
                 }
             }
         }
-        return false;
+
+        // Check tl-br
+        // 0 <= c <= N-1, 0 <= r <= M-1, -N+1 <= i <= M-1
+        // 0 <= r = c + i <= min(N-1+i, M-1)
+        for (int i = -N+1; i <= M-1; i++) {
+            Cell::CellKey pp = Cell::PlayerNoneKey;
+            int pc = 0;
+            for (int r = 0; r <= std::min(N-1+i, M-1); r++) {
+                int c = r - i;
+                if (check_loop(req, grid[r][c], pp, pc)) {
+                    for (int _r = r - req + 1; _r <= r; _r++) {
+                        grid[_r][_r - i].mark();
+                    }
+                    return pp;
+                }
+            }
+        }
+
+        // Check tr-bl
+        // 0 <= c <= N-1, 0 <= r <= M-1, 0 <= i <= M+N-1
+        // 0 <= r = i - c <= i
+        for (int i = 0; i <= M+N-1; i++) {
+            Cell::CellKey pp = Cell::PlayerNoneKey;
+            int pc = 0;
+            for (int r = 0; r <= i; r++) {
+                int c = i - r;
+                if (check_loop(req, grid[r][c], pp, pc)) {
+                    for (int _r = r - req + 1; _r <= r; _r++) {
+                        grid[_r][i - _r].mark();
+                    }
+                    return pp;
+                }
+            }
+        }
+        return Cell::PlayerNoneKey;
     }
+
+    private:
+        bool check_loop(int req, Cell p, Cell::CellKey& pp, int& pc) {
+            if (p.key == pp) {
+                pc++;
+            } else {
+                pp = p.key;
+                pc = 1;
+            }
+            if (pc >= req && pp != Cell::PlayerNoneKey) {
+                return true;
+            }
+            return false;
+        }
 };
 
 #endif
--- 3-connect4/lib/utils.hpp
+++ 3-connect4/lib/utils.hpp
@@ -4,7 +4,7 @@
 #include <SDL2/SDL.h>
 
 template <typename T>
-T* unsafe_shift(T* ptr, int n_bytes) {
+T* unsafeShift(T* ptr, int n_bytes) {
     return reinterpret_cast<T*>(reinterpret_cast<uint8_t*>(ptr) + n_bytes);
 }
 
@@ -15,28 +15,8 @@ struct Color {
     int a = 255;
 };
 
-Color WHITE{255, 255, 255};
-Color RED{255, 0, 0};
-Color BLUE{0, 0, 255};
-Color BOARD_BG{0, 0, 100};
-
-enum class Player {
-    None,
-    PlayerRed,
-    PlayerBlue,
-};
-
-uint32_t map_color(SDL_PixelFormat* format, Color& color) {
+uint32_t mapColor(SDL_PixelFormat* format, Color color) {
     return SDL_MapRGBA(format, color.r, color.g, color.b, color.a);
-}
-
-inline std::ostream& operator<<(std::ostream& os, Player p) {
-    switch (p) {
-        case Player::PlayerRed:  return os << "R";
-        case Player::PlayerBlue: return os << "B";
-        default:                 return os << ".";
-    }
-    return os;
 }
 
 template <typename T, size_t M, size_t N>
