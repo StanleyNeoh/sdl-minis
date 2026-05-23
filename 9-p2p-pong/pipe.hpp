f35806f5dc86d9e558dfe8975715751c0f8a05d1
stanleyneoh <neohstanley@gmail.com>
2026-05-23 20:20:44 +0800

    Used cached head and tail

--- 9-p2p-pong/pipe.hpp
+++ 9-p2p-pong/pipe.hpp
@@ -13,27 +13,31 @@ struct SPSCQueue {
     alignas(64) std::array<T, N> buffer;
     alignas(64) std::atomic<size_t> head = 0;
     alignas(64) std::atomic<size_t> tail = 0;
+    alignas(64) size_t cached_head = 0;
+    alignas(64) size_t cached_tail = 0;
 
-    bool push(const T& item) noexcept {
+    bool try_push(const T& item) noexcept {
         size_t t = tail.load(std::memory_order_relaxed);
-        size_t h = head.load(std::memory_order_acquire);
-        if (t - h == N) return false;
+        if (t - cached_head >= N) {
+            cached_head = head.load(std::memory_order_acquire);
+            if (t - cached_head >= N) return false;
+        }
         buffer[t & mask] = item;
         tail.store(t+1, std::memory_order_release);
         return true;
     }
 
-    bool pop(T& item) noexcept {
+    bool try_pop(T& item) noexcept {
         return pop_impl<true>(&item);
     }
 
-    bool pop() noexcept {
+    bool try_pop() noexcept {
         return pop_impl<false>(nullptr);
     }
     
     bool clear() noexcept {
         bool cleared = false;
-        while (pop()) {
+        while (try_pop()) {
             cleared = true;
         }
         return cleared;
@@ -41,10 +45,12 @@ struct SPSCQueue {
 
     private:
         template <bool CopyValue>
-        bool pop_impl(T* item) noexcept {
-            size_t t = tail.load(std::memory_order_acquire);
+        bool try_pop_impl(T* item) noexcept {
             size_t h = head.load(std::memory_order_relaxed);
-            if (t - h == 0) return false;
+            if (cached_tail == h) {
+                cached_tail = tail.load(std::memory_order_acquire);
+                if (cached_tail = h) return false;
+            }
             if constexpr (CopyValue) {
                 *item = buffer[h & mask];
             }
