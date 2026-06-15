03247c70a05c267864ef28bdc356cfe8bddd6cb0
stanleyneoh <neohstanley@gmail.com>
2026-06-15 21:06:42 +0800

    Add import all files to libs

---
+++ 9-p2p-pong/lib/common/common.hpp
@@ -0,0 +1,9 @@
+#ifndef COMMON_COMMON_HPP
+#define COMMON_COMMON_HPP
+
+#include "logger.hpp"
+#include "pipe.hpp"
+#include "platform_socket.hpp"
+#include "utils.hpp"
+
+#endif
---
+++ 9-p2p-pong/lib/p2p/p2p.hpp
@@ -0,0 +1,9 @@
+#ifndef P2P_P2P_HPP
+#define P2P_P2P_HPP
+
+#include "packet.hpp"
+#include "reader.hpp"
+#include "tcp_manager.hpp"
+#include "type.hpp"
+
+#endif
